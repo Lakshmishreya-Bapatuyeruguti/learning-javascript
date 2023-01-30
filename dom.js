b6beb7126427decbb1cc0e7a469ae7ee74cb1ced
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-01-30 18:39:27 +0530

    added onclick function to button

--- dom.js
+++ dom.js
@@ -1,2 +1,7 @@
 document.getElementById("greetDOM").innerHTML += "DOM ";
 document.getElementById("para").style.color = "red";
+
+function changeColFun() {
+  document.getElementById("btn").style.backgroundColor = "blue";
+  document.getElementById("btn").style.color = "white";
+}
