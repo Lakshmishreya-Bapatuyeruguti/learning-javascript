60446186c1fb1aebe711e5e450d4a39348d945a4
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-01-30 14:18:12 +0530

    reduce method

--- advArrayMethods.js
+++ advArrayMethods.js
@@ -1,4 +1,4 @@
-//For each - takes a callback and performs callback function on each of element present in array
+//For each
 // forEach(callback(element, index)) - default syntax
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -19,7 +19,7 @@ numbers.forEach((number) => {
   console.log(number * 100);
 });
 
-//Map Method -takes callback and returns new array and return keyword is must
+//Map Method
 
 const newArr = numbers.map((number) => {
   return number * 2;
@@ -33,3 +33,16 @@ const filtered = numbers.filter((number) => {
   return number % 2 == 0;
 });
 console.log(filtered);
+
+// Reduce Method
+
+const users = [
+  { id: 1, productName: "bottle", quantity: 2 },
+  { id: 1, productName: "car", quantity: 4 },
+  { id: 1, productName: "doormat", quantity: 6 },
+];
+const total = users.reduce((acc, curVal) => {
+  return acc + curVal.quantity;
+}, 0);
+//above 0 is optional value to set acc start as 0 coz by default its first element
+console.log(total);
