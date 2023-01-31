5e8cee53484526f20fbb11eae9bfb230f5c779f0
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-01-31 10:23:07 +0530

    adding styles

--- dom.js
+++ dom.js
@@ -1,6 +1,9 @@
-document.getElementById("greetDOM").innerHTML += "DOM ";
+// selecting element by id
+document.getElementById("greetDOM").innerHTML += " DOM ";
 document.getElementById("para").style.color = "red";
-
+//console logging our document object
+console.dir(document);
+// to change button style on click
 function changeColFun() {
   document.getElementById("btn").style.backgroundColor = "blue";
   document.getElementById("btn").style.color = "white";
