6bd3f87fbba2c2020fe192d7264dbe93b2aadae1
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-01-31 16:18:19 +0530

    understanding dom

--- dom.js
+++ dom.js
@@ -1,10 +1,63 @@
-// selecting element by id
-document.getElementById("greetDOM").innerHTML += " DOM ";
-document.getElementById("para").style.color = "red";
 //console logging our document object
 console.dir(document);
-// to change button style on click
+
+// selecting element by id
+document.getElementById("greetDOM").innerHTML += " DOM ";
+
+//selecting element by class
+let collec = (document.getElementsByClassName("pTag")[0].style.color = "red");
+
+//queryselector -return first class
+let item = document.querySelector(".item");
+console.log(item);
+
+//queryselectorAll - Returns all as a node list
+let items = document.querySelectorAll(".item");
+console.log(items);
+
+//targeting one of the list item
+document.querySelectorAll(".item")[3].style.backgroundColor = "lightgreen";
+
+//selecting by tag
+let collTag = document.getElementsByTagName("li");
+console.log(collTag);
+
+//innerHTML
+document.querySelector("h2").innerHTML += " - Appending content";
+document.querySelector("h3").innerHTML = " Overwriting content ";
+document.querySelector("h4").innerHTML =
+  "<p class=secHeading> Paragragh tag added using innerHTML</p>";
+
+//classList
+let cList = document.querySelector("#greetDOM").classList;
+console.log(cList);
+//add class
+cList.add("welcome");
+//remove class
+cList.remove("welcome");
+// toggle class
+cList.toggle("welcome");
+cList.toggle("welcome");
+
+//create element
+
+let createListItem = document.createElement("li");
+createListItem.textContent = "added element using create element";
+let listItem = document.querySelector(".itemList");
+//append
+listItem.appendChild(createListItem);
+//prepend
+listItem.prepend(createListItem);
+
+//getting dimensions of an element
+
+let info = document.querySelector("#btn").getBoundingClientRect();
+console.log(info);
+
+// to change button style on click without eventListener
 function changeColFun() {
   document.getElementById("btn").style.backgroundColor = "blue";
-  document.getElementById("btn").style.color = "white";
+  document.getElementById("btn").style.color = "white ";
 }
+
+//to change button style on click with eventListener
