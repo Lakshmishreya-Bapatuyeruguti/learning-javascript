633093446ac31a8d7ec9451add2d8797deacd03e
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-02-01 18:50:55 +0530

    Inheritance and getters and setters

--- classes.js
+++ classes.js
@@ -1,4 +1,5 @@
 // class
+
 class StudentList {
   constructor(sName, sAge, SDob) {
     this.entryName = sName;
@@ -12,3 +13,31 @@ class StudentList {
 let stud = new StudentList("shreya");
 console.log(stud);
 console.log(stud.desc());
+
+//Inheritance
+
+class PrimaryList extends StudentList {
+  constructor(sName, sAge, SDob, guardian) {
+    //super keyword
+    super(sName, sAge, SDob);
+    this.parent = guardian;
+  }
+  //getters
+  get care() {
+    return `parent is: ${this.parent}`;
+  }
+  //setters
+  set combo(kidNewName) {
+    this.entryName = kidNewName;
+  }
+  //method overriding
+  desc() {
+    return `kid is ${this.entryName}`;
+  }
+}
+let kid = new PrimaryList("kris", 5, 2019, "vasu");
+console.log(kid);
+console.log(kid.desc());
+console.log(kid.care);
+kid.combo = "shiv";
+console.log(kid);
