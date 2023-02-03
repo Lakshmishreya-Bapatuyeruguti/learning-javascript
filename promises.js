c5c1cf5ae784184c390a24cdea57f1defcc9e18c
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-02-03 14:49:32 +0530

    promises and async-await

--- promises.js
+++ promises.js
@@ -1,36 +1,36 @@
 //set Timeout
 
-// console.log("start");
-// setTimeout(() => {
-//   console.log("inside set timeout -0sec");
-// }, 0);
-// for (let i = 0; i < 10000; i++) {
-//   console.log("....");
-// }
-// console.log("end!");
+console.log("start");
+setTimeout(() => {
+  console.log("inside set timeout -0sec");
+}, 0);
+for (let i = 0; i < 10000; i++) {
+  console.log("....");
+}
+console.log("end!");
 
 //clearing Timeout
 
-// console.log("start 2");
-// const id = setTimeout(() => {
-//   console.log("inside set timeout-2sec ");
-// }, 2000);
-// for (let i = 0; i < 10000; i++) {
-//   console.log("....");
-// }
-// clearTimeout(id);
-// console.log("end! 2");
+console.log("start 2");
+const id = setTimeout(() => {
+  console.log("inside set timeout-2sec ");
+}, 2000);
+for (let i = 0; i < 10000; i++) {
+  console.log("....");
+}
+clearTimeout(id);
+console.log("end! 2");
 
 //set Interval
 
-// console.log("BeforeInterval");
-// setInterval(() => {
-//   console.log("Hi");
-// }, 1000);
-// for (let i = 0; i < 10000; i++) {
-//   console.log("....");
-// }
-// console.log("After Interval");
+console.log("BeforeInterval");
+setInterval(() => {
+  console.log("Hi");
+}, 1000);
+for (let i = 0; i < 10000; i++) {
+  console.log("....");
+}
+console.log("After Interval");
 
 // callbacks in asynchronous programming
 
